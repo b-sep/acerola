7c5968a8f5bc1a6d97ad8f3cc4bbfabff6576d47
Júnior <b.sep@live.com>
2024-02-10 15:58:00 -0300

    valida parametros

--- post_params_validator.rb
+++ post_params_validator.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
 module PostParamsValidator
-  def self.exec(id:, valor:, tipo:, descricao:)
+  def self.exec(**params)
+    return false unless params[:id] && params[:valor] && params[:tipo] && params[:descricao]
+
+    id = params[:id]
+    value = params[:valor]
+    type = params[:tipo]
+    description = params[:descricao]
+
     return false if id.match?(/\D/)
-    return false unless valor.instance_of?(Integer)
-    return false if valor.zero? || valor.negative?
-    return false unless tipo.instance_of?(String)
-    return false unless %w[c d].include?(tipo.downcase)
-    return false unless descricao.instance_of?(String)
-    return false if descricao.empty? || descricao.size > 10
+    return false unless value.instance_of?(Integer)
+    return false if value.zero? || value.negative?
+    return false unless type.instance_of?(String)
+    return false unless %w[c d].include?(type.downcase)
+    return false unless description.instance_of?(String)
+    return false if description.empty? || description.size > 10
 
     true
   end
